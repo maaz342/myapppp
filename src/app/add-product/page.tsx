68d025037f89f7e8c868ea090c60b01736853006
chames@gmail.com <maazkamran72@gmail.com>
2023-11-20 06:34:18 +0500

    dais

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,59 @@
+import { prisma } from '@/lib/db/prisma';
+import { redirect } from 'next/navigation';
+import React from 'react'
+export const metadata = {
+  title: "Add Product - Flowmazon",
+};
+async function addProduct(formData: FormData) {
+  "use server";
+  const name=formData.get("name")?.toString()
+  const description=formData.get("description")?.toString()
+  const imageUrl=formData.get("imageUrl")?.toString()
+  const price=Number(formData.get("price")||0)
+  if(!name||!description||!imageUrl||!price){
+    throw Error("MISSING REQUIRED FIELDS")
+  }
+
+
+await prisma.product.create({
+  data:{name,description, imageUrl,price},
+})
+redirect
+  }
+export default function page() {
+  return (
+    <div>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form action={addProduct}>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input-bordered input mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea-bordered textarea mb-3 w-full"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="input-bordered input mb-3 w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input-bordered input mb-3 w-full"
+        />
+        <button className=" btn btn-block btn-primary ">Add Product</button>
+      </form>
+    </div>
+  );
+}
+
